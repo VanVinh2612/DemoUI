265181be533e582b855ba5c6ec58dc87a0e7de39
VanVinh2612 <57625733+VanVinh2612@users.noreply.github.com>
2023-04-07 17:19:55 +0700

    add detail item tabview

--- DemoUi/Tourism.swift
+++ DemoUi/Tourism.swift
@@ -8,16 +8,17 @@
 import UIKit
 
 struct Tourism {
-    let title: String
+    let id: Int
+    var title: String
     let imageName: String
 }
 
-let data: [Tourism] = [
-    Tourism(title: "Bán đảo Sơn Trà", imageName: "ban_dao_son_tra_img"),
-    Tourism(title: "Đảo Phú Quốc", imageName: "dao_phu_quoc_img"),
-    Tourism(title: "Du lịch Vịnh Hạ Long", imageName: "du_lich_vinh_Ha_Long_img"),
-    Tourism(title: "Hội An", imageName: "hoi_an_img"),
-    Tourism(title: "Nhà Thờ Đức Bà", imageName: "nha_tho_duc_ba_img"),
-    Tourism(title: "Biền Nhà Trang", imageName: "nha_trang_beaches_img"),
-    Tourism(title: "Vinwonders Nam Hội An", imageName: "vinwonders_nam_hoi_an_img"),
+var data: [Tourism] = [
+    Tourism(id: 1, title: "Bán đảo Sơn Trà", imageName: "ban_dao_son_tra_img"),
+    Tourism(id: 2, title: "Đảo Phú Quốc", imageName: "dao_phu_quoc_img"),
+    Tourism(id: 3, title: "Du lịch Vịnh Hạ Long", imageName: "du_lich_vinh_Ha_Long_img"),
+    Tourism(id: 4, title: "Hội An", imageName: "hoi_an_img"),
+    Tourism(id: 5, title: "Nhà Thờ Đức Bà", imageName: "nha_tho_duc_ba_img"),
+    Tourism(id: 6, title: "Biền Nhà Trang", imageName: "nha_trang_beaches_img"),
+    Tourism(id: 7, title: "Vinwonders Nam Hội An", imageName: "vinwonders_nam_hoi_an_img"),
 ]
